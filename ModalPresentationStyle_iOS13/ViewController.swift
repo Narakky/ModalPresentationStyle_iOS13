dcda59811f65806fd314cd06373c0e35a9d9797a
Narakky <11222997+Narakky@users.noreply.github.com>
2019-08-08 11:07:31 +0900

    initial commit

---
+++ ModalPresentationStyle_iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModalPresentationStyle_iOS13
+//
+//  Created by Naraki on 8/8/19.
+//  Copyright © 2019 i-enter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
