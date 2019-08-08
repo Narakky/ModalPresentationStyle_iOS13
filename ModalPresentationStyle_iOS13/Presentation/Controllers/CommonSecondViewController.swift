df904e8664e934be7d4a0d7b760df386c85260ff
Narakky <11222997+Narakky@users.noreply.github.com>
2019-08-08 16:28:41 +0900

    普通にPresentModalするViewControllerを作った

---
+++ ModalPresentationStyle_iOS13/Presentation/Controllers/CommonSecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommonSecondViewController.swift
+//  ModalPresentationStyle_iOS13
+//
+//  Created by Naraki on 8/8/19.
+//  Copyright © 2019 i-enter. All rights reserved.
+//
+
+import UIKit
+
+final class CommonSecondViewController: UIViewController {
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- ModalPresentationStyle_iOS13/Presentation/Controllers/ViewController.swift
+++ ModalPresentationStyle_iOS13/Presentation/Controllers/ViewController.swift
@@ -10,10 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Definitions
+
+    private enum TableData: CaseIterable {
+        case common
+        case hasGesture
+
+        var title: String {
+            switch self {
+            case .common: return "普通"
+            case .hasGesture: return "ジェスチャ有り"
+            }
+        }
+
+        var detail: String {
+            switch self {
+            case .common: return "何の変哲もない普通の画面に遷移します"
+            case .hasGesture: return "ジェスチャが存在する画面に遷移します"
+            }
+        }
+    }
+
+    // MARK: - Outlets
+
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.tableFooterView = UIView()
+            tableView.separatorInset = .zero
+        }
+    }
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: selectedIndexPath, animated: false)
+        }
+    }
 
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableData.allCases.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+        let data = TableData.allCases[indexPath.row]
+        cell.textLabel?.text = data.title
+        cell.detailTextLabel?.text = data.detail
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let data = TableData.allCases[indexPath.row]
+
+        switch data {
+        case .common:
+            if let secondViewController = UIStoryboard(name: "CommonSecondViewController", bundle: nil).instantiateInitialViewController() {
+                navigationController?.pushViewController(secondViewController, animated: true)
+            }
+        default:
+            break
+        }
+    }
+}
